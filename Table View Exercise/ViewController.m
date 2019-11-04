039dda4475fec8955f735f5fe119e5111de2151a
JM Sumague <jm.sumague@opsolutions.biz>
2019-11-04 15:04:16 +0800

    Initial Commit

---
+++ Table View Exercise/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Table View Exercise
+//
+//  Created by OPSolutions on 04/11/2019.
+//  Copyright © 2019 OPSolutions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
